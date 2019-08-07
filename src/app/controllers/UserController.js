dad2a96a59a8ccd6b3f67a0983840dd56d005a6f
Andson ferreira <andferreira92@outlook.com>
2019-08-07 09:40:27 -0300

    FirstCommit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    /**
+     * emailExists => Verifica se o email já esta cadastrado
+     */
+    const emailExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (emailExists) {
+      return res.status(400).json({ error: 'Email já cadastrado' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
