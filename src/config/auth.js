31f21a70c3f7cd28e2d65d1e7d51e65959d2ac81
Andson ferreira <andferreira92@outlook.com>
2019-08-16 21:17:50 -0300

    ADD-env-configurations

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'efecabef3f5ba364368bdedd5ea3d056',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: '2a973c238ee3e5',
-    pass: '0a42989817f46a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Organização Meetup <noreply@meetups.com>',
